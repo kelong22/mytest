a1afbdc58148f3d6900317ecb8477ca1f95efffb
KeLong <1402994031@qq.com>
2020-02-24 17:40:23 +0800

    第一次提交到本地仓库

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * @author 周盼龙
+ * @date 2020/2/24 17:35
+ */
+@Data
+public class User {
+    private int id;
+    private String username;
+}
